3e9a0d259cd2f68ebce348d5f55905c6d2e945d9
jjf <fentonjoe@hotmail.com>
2014-10-04 16:17:56 +0100

    missed

---
+++ Sudoku/src/joephysics62/co/uk/sudoku/creator/util/CellFilterForArrayPuzzle.java
@@ -0,0 +1,29 @@
+package joephysics62.co.uk.sudoku.creator.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import joephysics62.co.uk.sudoku.model.Coord;
+import joephysics62.co.uk.sudoku.model.Layout;
+import joephysics62.co.uk.sudoku.model.Puzzle;
+import joephysics62.co.uk.sudoku.solver.CellFilter;
+
+public abstract class CellFilterForArrayPuzzle implements CellFilter {
+
+  @Override
+  public final List<Coord> apply(Puzzle puzzle) {
+    List<Coord> unsolved = new ArrayList<>();
+    int[][] allCells = puzzle.getAllCells();
+    Layout layout = puzzle.getLayout();
+    for (int rowNum = 1; rowNum <= layout.getHeight(); rowNum++) {
+      for (int colNum = 1; colNum <= layout.getWidth(); colNum++) {
+        if (accept(allCells[rowNum - 1][colNum -1])) {
+          unsolved.add(Coord.of(rowNum, colNum));
+        }
+      }
+    }
+    return unsolved;
+  }
+
+  protected abstract boolean accept(int cellValue);
+}
