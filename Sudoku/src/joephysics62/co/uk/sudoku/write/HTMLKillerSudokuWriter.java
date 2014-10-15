30a6fdf0cf60d552211b55ed0beecab9f480be38
jjf <fentonjoe@hotmail.com>
2014-10-15 08:29:48 +0100

    Plausible if somewhat ugly killer sudoku writer

---
+++ Sudoku/src/joephysics62/co/uk/sudoku/write/HTMLKillerSudokuWriter.java
@@ -0,0 +1,101 @@
+package joephysics62.co.uk.sudoku.write;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import joephysics62.co.uk.sudoku.constraints.Constraint;
+import joephysics62.co.uk.sudoku.constraints.UniqueSum;
+import joephysics62.co.uk.sudoku.model.Coord;
+import joephysics62.co.uk.sudoku.model.Layout;
+import joephysics62.co.uk.sudoku.model.Puzzle;
+
+public class HTMLKillerSudokuWriter extends HTMLWriter {
+
+  private static final String TEMPLATE_FTL = "templates/killerSudokuTemplate.ftl";
+
+  public HTMLKillerSudokuWriter(Puzzle puzzle) {
+    super(puzzle, TEMPLATE_FTL);
+  }
+
+  @Override
+  protected void addPuzzleSpecificParams(Map<String, Object> root, final Layout layout) {
+    root.put("subTableHeight", layout.getSubTableHeight());
+    root.put("subTableWidth", layout.getSubTableWidth());
+  }
+
+  protected String getTemplateLocation() {
+    return TEMPLATE_FTL;
+  }
+
+  public static class KillerSudokuCell {
+    private final Integer _sum;
+    private final int _groupId;
+
+    public KillerSudokuCell(final Integer sum, final int groupId) {
+      _sum = sum;
+      _groupId = groupId;
+    }
+
+    public int getGroupId() { return _groupId; }
+    public Integer getSum() { return _sum; }
+  }
+
+  @Override
+  protected List<List<Object>> generateTable() {
+    Puzzle puzzle = getPuzzle();
+    Layout layout = puzzle.getLayout();
+    List<List<Object>> table = new ArrayList<>();
+    for (int rowNum = 1; rowNum <= layout.getHeight(); rowNum++) {
+      final List<Object> rowList = new ArrayList<>();
+      for (int colNum = 1; colNum <= layout.getWidth(); colNum++) {
+        Coord coord = Coord.of(rowNum, colNum);
+        final int groupId = getGroupId(coord);
+        final Integer sum = getSum(groupId, coord);
+        rowList.add(new KillerSudokuCell(sum, groupId));
+      }
+      table.add(rowList);
+    }
+    return table;
+  }
+
+  private Integer getSum(int groupId, Coord coord) {
+    Constraint constraint = getPuzzle().getVariableConstraints().get(groupId);
+    if (constraint instanceof UniqueSum) {
+      UniqueSum uniqueSum = (UniqueSum) constraint;
+      List<Coord> cellsInUniqueSum = uniqueSum.getCells();
+      if (!cellsInUniqueSum.contains(coord)) {
+        throw new RuntimeException();
+      }
+      List<Integer> rows = new ArrayList<>();
+      List<Integer> cols = new ArrayList<>();
+      for (Coord coord2 : cellsInUniqueSum) {
+        rows.add(coord2.getRow());
+        cols.add(coord2.getCol());
+      }
+      Collections.sort(rows);
+      Collections.sort(cols);
+      if (coord.equals(Coord.of(rows.get(0), cols.get(0)))) {
+        return uniqueSum.getSum();
+      }
+      else {
+        return null;
+      }
+    }
+    else {
+      throw new RuntimeException();
+    }
+  }
+
+  private int getGroupId(Coord coord) {
+    for (Constraint constraint : getPuzzle().getConstraints(coord)) {
+      int indexOf = getPuzzle().getVariableConstraints().indexOf(constraint);
+      if (indexOf >= 0) {
+        return indexOf;
+      }
+    }
+    throw new RuntimeException();
+  }
+
+}
---
+++ Sudoku/templates/killerSudokuTemplate.ftl
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<title>${title}</title>
+<style>
+* {
+  font-family: Arial;
+}
+td, th {
+    border-color: black;
+    border-width: thin;
+    border-style: solid;
+    border-spacing: 0px;
+    width: 1em;
+    height:1em;
+    text-align:  center;
+}
+tr:nth-child(1) td {
+    border-top-width: medium;
+}
+td:nth-child(1) {
+    border-left-width: medium;
+}
+tr:nth-child(${subTableHeight}n + 0) td {
+    border-bottom-width: medium;
+}
+td:nth-child(${subTableWidth}n + 0) {
+    border-right-width: medium;
+}
+td div {
+  font-size: xx-small;
+  position: relative;
+  left: -0.7em;
+  bottom: 0.8em;
+}
+table {
+    border-collapse: collapse;
+}
+<#list AGroup as AGroups>
+td.group_${AGroup},
+</#list>
+<#if AGroups??>
+{
+  background-color: rgb(250,190,190);
+}
+</#if>
+<#list BGroup as BGroups>
+td.group_${BGroup},
+</#list>
+<#if BGroups??>
+{
+  background-color: rgb(190,250,190);
+}
+</#if>
+<#list CGroup as CGroups>
+td.group_${CGroup},
+</#list>
+<#if CGroups??>
+{
+  background-color: rgb(190,190,250);
+}
+</#if>
+<#list DGroup as DGroups>
+td.group_${DGroup},
+</#list>
+<#if DGroups??>
+{
+  background-color: rgb(250,190,250);
+}
+</#if>
+</style>
+</head>
+<body>
+  <table>
+ <#list table as row>
+   <tr> 
+   <#list row as cell>
+     <td class="group_${cell.groupId}">
+       <#if cell.sum??><div>${cell.sum}</div></#if>
+     </td>
+   </#list>
+   </tr>
+ </#list>
+ </table>
+</body>
+
+</html>
