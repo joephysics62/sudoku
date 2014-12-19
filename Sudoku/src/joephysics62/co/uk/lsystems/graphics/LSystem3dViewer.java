9dcd5603c09660dc5917586a7b5309febb72d2da
joephyics62 <fentonjoe@hotmail.com>
2014-12-19 15:45:54 +0000

    Extract the viewing class from the 3d generation

---
+++ Sudoku/src/joephysics62/co/uk/lsystems/graphics/LSystem3dViewer.java
@@ -0,0 +1,30 @@
+package joephysics62.co.uk.lsystems.graphics;
+
+import javafx.scene.Group;
+import joephysics62.co.uk.lsystems.LSystemTurtleInterpreter;
+import joephysics62.co.uk.lsystems.Line3D;
+import joephysics62.co.uk.lsystems.examples.PythagoreanTree3d;
+import joephysics62.co.uk.lsystems.turtle.TurtleLSystem;
+
+public class LSystem3dViewer extends Simple3dViewer {
+
+  private static final TurtleLSystem<Character> LSYSTEM = new PythagoreanTree3d();
+  private static final int ITERATIONS = 5;
+
+  @Override
+  protected Group createContentGroup() {
+    final Group output = new Group();
+
+    final LSystemTurtleInterpreter turtleInterpreter = new LSystemTurtleInterpreter(LSYSTEM);
+    for (final Line3D line3d : turtleInterpreter.interpret(LSYSTEM.generate(ITERATIONS))) {
+      final double radius = line3d.getWidth() / 2;
+      output.getChildren().add(new ConnectingCylinder(line3d.getStart(), line3d.getEnd(), line3d.getColour(), radius));
+    }
+    return output;
+  }
+
+  public static void main(final String[] args) {
+    launch(args);
+  }
+
+}
--- Sudoku/src/joephysics62/co/uk/lsystems/graphics/Simple3dViewer.java
+++ Sudoku/src/joephysics62/co/uk/lsystems/graphics/Simple3dViewer.java
@@ -12,22 +12,15 @@ import javafx.scene.paint.Color;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
-import joephysics62.co.uk.lsystems.LSystemTurtleInterpreter;
-import joephysics62.co.uk.lsystems.Line3D;
-import joephysics62.co.uk.lsystems.examples.PythagoreanTree3d;
-import joephysics62.co.uk.lsystems.turtle.TurtleLSystem;
 
-public class LSystem3dWriter extends Application {
-
-  private static final TurtleLSystem<Character> LSYSTEM = new PythagoreanTree3d();
-  private static final int ITERATIONS = 5;
+public abstract class Simple3dViewer extends Application {
 
   private PerspectiveCamera _camera;
 
   @Override
   public void start(final Stage primaryStage) throws Exception {
     primaryStage.setResizable(false);
-    final Scene scene = new Scene(createContent(LSYSTEM, ITERATIONS));
+    final Scene scene = new Scene(createContent());
     scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
 
       @Override
@@ -35,11 +28,9 @@ public class LSystem3dWriter extends Application {
         switch (keyEvent.getCode()) {
         case Z:
           _camera.getTransforms().add(new Translate(0, 0, 1));
-          _distance--;
           break;
         case A:
           _camera.getTransforms().add(new Translate(0, 0, -1));
-          _distance++;
           break;
         case LEFT:
           _output.getTransforms().add(new Rotate(-5, Rotate.Z_AXIS));
@@ -62,40 +53,32 @@ public class LSystem3dWriter extends Application {
     primaryStage.show();
   }
 
-  private double _distance = 15;
+  private static final double START_DISTANCE = 15;
 
   private Group _output;
 
-  private Parent createContent(final TurtleLSystem<Character> lsystem, final int iterations) {
+  private Parent createContent() {
     _camera = new PerspectiveCamera(true);
     _camera.getTransforms().add(new Rotate(5, Rotate.Z_AXIS));
     _camera.getTransforms().add(new Rotate(-115, Rotate.X_AXIS));
-    _camera.getTransforms().add(new Translate(0, -2, -_distance));
+    _camera.getTransforms().add(new Translate(0, -2, -START_DISTANCE));
 
     // Build the Scene Graph
     final Group root = new Group();
     root.getChildren().add(_camera);
 
-    _output = new Group();
+    _output = createContentGroup();
     root.getChildren().add(_output);
 
-    final LSystemTurtleInterpreter turtleInterpreter = new LSystemTurtleInterpreter(lsystem);
-    for (final Line3D line3d : turtleInterpreter.interpret(lsystem.generate(iterations))) {
-      final double radius = line3d.getWidth() / 2;
-      _output.getChildren().add(new ConnectingCylinder(line3d.getStart(), line3d.getEnd(), line3d.getColour(), radius));
-    }
     // Use a SubScene
-    final SubScene subScene = new SubScene(root, 600, 600);
-    subScene.setFill(Color.WHITE);
+    final SubScene subScene = new SubScene(root, 800, 600);
+    subScene.setFill(Color.BLACK);
     subScene.setCamera(_camera);
     final Group group = new Group();
     group.getChildren().add(subScene);
     return group;
   }
 
-
-  public static void main(final String[] args) {
-    launch(args);
-  }
+  protected abstract Group createContentGroup();
 
 }
