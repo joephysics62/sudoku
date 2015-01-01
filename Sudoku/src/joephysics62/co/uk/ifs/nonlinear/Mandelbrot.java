e2155a26d5a257270f8081bd15701db6244cf147
joephyics62 <fentonjoe@hotmail.com>
2015-01-01 15:18:23 +0000

    extract useful class

--- Sudoku/src/joephysics62/co/uk/ifs/nonlinear/Mandelbrot.java
+++ Sudoku/src/joephysics62/co/uk/ifs/nonlinear/Mandelbrot.java
@@ -2,13 +2,9 @@ package joephysics62.co.uk.ifs.nonlinear;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.stream.FileImageOutputStream;
-import javax.imageio.stream.ImageOutputStream;
-
-import joephysics62.co.uk.lsystems.animation.GifSequenceWriter;
+import joephysics62.co.uk.plotting.AnimatedPlottingWriter;
 import joephysics62.co.uk.plotting.ImageScale;
 
 import org.apache.commons.math3.complex.Complex;
@@ -18,44 +14,47 @@ public class Mandelbrot {
   private static int MAX_ITER = 30;
 
   public static void main(final String[] args) throws IOException {
-    final double ymin = -1;
-    final double ymax = 1;
-    final double xmin = -2;
+    final double ymin = -2;
+    final double ymax = 2;
+    final double xmin = -3;
     final ImageScale imageScale = new ImageScale(600, 1.5, ymin, ymax, xmin);
 
     final double escapeLimit = 15;
 
-    final String fileName = "C:\\Users\\joe\\Pictures\\mandelbrot.gif";
+    final String fileName = "C:\\Users\\joe\\Pictures\\mandelbrot4.gif";
 
-    final int numFrames = 240;
-    try (final ImageOutputStream outputStream = new FileImageOutputStream(new File(fileName))) {
-      final GifSequenceWriter gifSequenceWriter = new GifSequenceWriter(outputStream, BufferedImage.TYPE_INT_RGB, 1000 / 24, true);
-
-      for (int i = 0; i < numFrames; i++) {
+    final AnimatedPlottingWriter animatedPlottingWriter = new AnimatedPlottingWriter(480, 24) {
+      @Override
+      protected BufferedImage createImage(final double progress) {
         final BufferedImage bi = new BufferedImage(imageScale.getWidth(), imageScale.getHeight(), BufferedImage.TYPE_INT_RGB);
         for (int x = 0; x < imageScale.getWidth(); x++) {
           for (int y = 0; y < imageScale.getHeight(); y++) {
             final Complex c = new Complex(imageScale.dataX(x), imageScale.dataY(y));
-            final double a = 1.0 * i / numFrames;
-            final Complex zNought = new Complex(a, a);
-            final double escapes = escapeTime(c, zNought, escapeLimit);
+            final double escapes = escapeTime(c, escapeLimit, progress);
             if (escapes >= 0) {
-              bi.setRGB(x, y, new Color(intensity(escapes), intensity(escapes), intensity(escapes)).getRGB());
+              bi.setRGB(x, y, toRGB(escapes));
             }
           }
         }
-        gifSequenceWriter.writeToSequence(bi);
-        System.out.println("Done FRAME " + i);
+        return bi;
       }
-    }
+    };
 
+    animatedPlottingWriter.write(fileName);
   }
 
-  private static int intensity(final double i) {
-    return Math.min(255, (int) (255.0 * i / MAX_ITER));
+  private static int toRGB(final double value) {
+    final int cycles = 2;
+    final double angle = 2 * cycles * Math.PI * value / MAX_ITER;
+    final int sine = (int) (255 * (Math.sin(angle) + 1) / 2.0);
+    final int cosine = (int) (255 * (Math.cos(angle) + 1) / 2.0);
+
+    return new Color(sine, cosine, 0).getRGB();
   }
 
-  private static double escapeTime(final Complex c, final Complex zNought, final double escapeLimit) {
+  private static double escapeTime(final Complex c, final double escapeLimit, final double animationProgress) {
+    final double a = 2.0 + 2.0 * animationProgress;
+    final Complex zNought = new Complex(0, 0);
     double currRe = zNought.getReal();
     double currIm = zNought.getImaginary();
     final double logOfMaxIterations = Math.log(MAX_ITER);
@@ -63,7 +62,7 @@ public class Mandelbrot {
 
     for (int i = 0; i < MAX_ITER; i++) {
       final double currReTemp = currRe * currRe - currIm * currIm + c.getReal();
-      currIm = 2.3 * currRe * currIm + c.getImaginary();
+      currIm = a * currRe * currIm + c.getImaginary();
       currRe = currReTemp;
       final double mod = Math.sqrt(currRe * currRe + currIm * currIm);
       if (mod > escapeLimit) {
---
+++ Sudoku/src/joephysics62/co/uk/plotting/AnimatedPlottingWriter.java
@@ -0,0 +1,36 @@
+package joephysics62.co.uk.plotting;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageOutputStream;
+
+import joephysics62.co.uk.lsystems.animation.GifSequenceWriter;
+
+public abstract class AnimatedPlottingWriter {
+
+  private final int _numFrames;
+  private final int _framesPerSecond;
+
+  public AnimatedPlottingWriter(final int numFrames, final int framesPerSecond) {
+    _numFrames = numFrames;
+    _framesPerSecond = framesPerSecond;
+  }
+
+  public void write(final String fileName) throws IOException {
+    final int timeBetweenFrames = 1000 / _framesPerSecond;
+    try (final ImageOutputStream outputStream = new FileImageOutputStream(new File(fileName))) {
+      final GifSequenceWriter gifSequenceWriter = new GifSequenceWriter(outputStream, BufferedImage.TYPE_INT_RGB, timeBetweenFrames, true);
+      for (int i = 0; i < _numFrames; i++) {
+        final BufferedImage bi = createImage(i / (1.0 * _numFrames));
+        gifSequenceWriter.writeToSequence(bi);
+        System.out.println("Done FRAME " + i);
+      }
+    }
+  }
+
+  protected abstract BufferedImage createImage(double animationProgress);
+
+}
