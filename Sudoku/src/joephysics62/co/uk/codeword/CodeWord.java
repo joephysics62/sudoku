f855554c79fc0a3e037f304a5f3617a4446df6a2
joephyics62 <fentonjoe@hotmail.com>
2016-02-27 13:09:27 +0000

    simplify codeword

--- Sudoku/src/joephysics62/co/uk/codeword/CodeWord.java
+++ Sudoku/src/joephysics62/co/uk/codeword/CodeWord.java
@@ -1,33 +1,84 @@
 package joephysics62.co.uk.codeword;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CodeWord {
-  private final CodeWordGrid _grid;
+
+  private static final int THRESHOLD = 25;
+
+  private final Dictionary _dictionary;
+
   private final CodeWordKey _key;
 
-  public CodeWord(final CodeWordGrid grid, final CodeWordKey key) {
-    _grid = grid;
-    _key = key;
+  private final Integer[][] _grid;
+  private final List<Word> _horizontals;
+  private final List<Word> _verticals;
+
+  private final Integer _height;
+  private final Integer _width;
+
+  private interface GridGetter {
+    Integer get(Integer[][] grid, int first, int second);
   }
 
-  public CodeWordGrid getGrid() {
-    return _grid;
+  public CodeWord(final CodeWordKey key, final int height, final int width, final Integer[][] grid, final Dictionary dictionary) throws IOException, URISyntaxException {
+    _key = key;
+    _height = height;
+    _width = width;
+    _grid = grid;
+    _dictionary = dictionary;
+    _horizontals = findWords(new GridGetter() {
+      @Override
+      public Integer get(final Integer[][] grid, final int first, final int second) {
+        return grid[first][second];
+      }
+    });
+    _verticals = findWords(new GridGetter() {
+      @Override
+      public Integer get(final Integer[][] grid, final int first, final int second) {
+        return grid[second][first];
+      }
+    });
   }
-  public CodeWordKey getKey() {
-    return _key;
+
+  private List<Word> findWords(final GridGetter gridGetter) {
+    final List<Word> words = new ArrayList<>();
+    for (int firstIndex = 0; firstIndex < _height; firstIndex++) {
+      List<Integer> currentWord = null;
+      for (int secondIndex = 0; secondIndex < _width; secondIndex++) {
+        final Integer cell = gridGetter.get(_grid, firstIndex, secondIndex);
+        if (cell != null) {
+          if (currentWord == null) {
+            currentWord = new ArrayList<Integer>();
+          }
+          currentWord.add(cell);
+        }
+        if (currentWord != null && (cell == null || secondIndex == _width - 1)) {
+          if (currentWord.size() > 1) {
+            words.add(new Word(currentWord));
+          }
+          currentWord = null;
+        }
+      }
+    }
+    return words;
   }
 
   @Override
   public String toString() {
     final StringBuilder sb = new StringBuilder();
-    for (final Integer[] keys : _grid.getGrid()) {
+    for (final Integer[] keys : _grid) {
       for (final Integer key : keys) {
         if (null == key) {
           sb.append("   ");
@@ -45,11 +96,11 @@ public class CodeWord {
     return sb.toString();
   }
 
-  public static CodeWord readFromFile(final String file) throws IOException {
+  public static CodeWord readFromFile(final String file) throws IOException, URISyntaxException {
     final List<String> allLines = Files.readAllLines(Paths.get(file));
     final CodeWordKey key = readKeyData(allLines.get(0));
-    final CodeWordGrid codeWordPuzzle = readGridData(allLines.subList(1, allLines.size()));
-    return new CodeWord(codeWordPuzzle, key);
+    final Integer[][] grid = readGridData(allLines.subList(1, allLines.size()));
+    return new CodeWord(key, 13, 13, grid, new Dictionary());
   }
 
   private static CodeWordKey readKeyData(final String keyData) {
@@ -63,7 +114,7 @@ public class CodeWord {
     return new CodeWordKey(keyMap);
   }
 
-  private static CodeWordGrid readGridData(final List<String> gridData) {
+  private static Integer[][] readGridData(final List<String> gridData) {
     Integer[][] codeWordPuzzle = null;
     int rowIndex = 0;
     for (final String line : gridData) {
@@ -76,7 +127,7 @@ public class CodeWord {
       }
       codeWordPuzzle[rowIndex++] = row;
     }
-    return new CodeWordGrid(codeWordPuzzle);
+    return codeWordPuzzle;
   }
 
   private static Integer toInt(final String string) {
@@ -85,5 +136,97 @@ public class CodeWord {
       return null;
     }
     return Integer.valueOf(trimmed);
+  }
+
+  public CodeWord solve() throws Exception {
+    final List<CodeWordKey> solutions = new ArrayList<>();
+    recurse(_key, solutions);
+    if (solutions.size() == 1) {
+      return new CodeWord(solutions.get(0), _height, _width, _grid, _dictionary);
+    }
+    throw new RuntimeException();
+  }
+
+  private void recurse(final CodeWordKey currentKey, final List<CodeWordKey> solutions) {
+    if (!isValid(currentKey)) {
+      return;
+    }
+    if (currentKey.isSolved()) {
+      solutions.add(currentKey);
+      return;
+    }
+    final Map<Word, List<String>> mapz = new LinkedHashMap<>();
+
+    for (final Word codeWordWord : getAllWords()) {
+      if (codeWordWord.isSolved(currentKey)) {
+        continue;
+      }
+      final List<String> matches = matches(codeWordWord, currentKey);
+      if (null != matches) {
+        mapz.put(codeWordWord, matches);
+      }
+    }
+    if (mapz.isEmpty()) {
+      System.err.println("Too many possibilies, not solvable");
+      return;
+    }
+
+    final Set<Entry<Word, List<String>>> entrySet = mapz.entrySet();
+
+    CodeWordKey basekey = currentKey;
+    Word bestGuesser = entrySet.iterator().next().getKey();
+    int bestSize = Integer.MAX_VALUE;
+    for (final Entry<Word, List<String>> entry : entrySet) {
+      final Word codeWordWord = entry.getKey();
+      final List<String> matches = entry.getValue();
+      if (matches.size() == 1) {
+        if (basekey.isViableMatch(codeWordWord, matches.get(0))) {
+          basekey = basekey.setWord(codeWordWord, matches.get(0));
+        }
+        else {
+          return;
+        }
+      }
+      else if (matches.size() > 1) {
+        if (matches.size() < bestSize) {
+          bestSize = matches.size();
+          bestGuesser = codeWordWord;
+        }
+      }
+    }
+    final List<String> matches = mapz.get(bestGuesser);
+    for (final String match : matches) {
+      if (basekey.isViableMatch(bestGuesser, match)) {
+        recurse(basekey.setWord(bestGuesser, match), solutions);
+      }
+    }
+
+  }
+
+  private List<Word> getAllWords() {
+    final List<Word> allWords = new ArrayList<Word>();
+    allWords.addAll(_horizontals);
+    allWords.addAll(_verticals);
+    return allWords;
+  }
+
+  private boolean isValid(final CodeWordKey newKey) {
+    for (final Word word : getAllWords()) {
+      final String setAnswer = word.getRegex(newKey);
+      final List<String> matches = _dictionary.matches(setAnswer, word.size(), THRESHOLD);
+      if (matches != null && matches.isEmpty()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private List<String> matches(final Word word, final CodeWordKey key) {
+    final String regex = word.getRegex(key);
+    final List<String> matches = _dictionary.matches(regex, word.size(), THRESHOLD);
+    if (matches.size() >= THRESHOLD) {
+      return null;
+    }
+    return matches.stream().filter(x -> key.isViableMatch(word, x)).collect(Collectors.toList());
   }
 }
--- Sudoku/src/joephysics62/co/uk/codeword/CodeWordMain.java
+++ Sudoku/src/joephysics62/co/uk/codeword/CodeWordMain.java
@@ -1,20 +1,15 @@
 package joephysics62.co.uk.codeword;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.List;
 
 public class CodeWordMain {
 
   private static final String FILE = "examples\\codeword\\codeWordTimes2635.txt";
 
-  public static void main(final String[] args) throws IOException, URISyntaxException {
+  public static void main(final String[] args) throws Exception {
     final String file = FILE;
     final CodeWord puzzle = CodeWord.readFromFile(file);
-    final CodewordSolver codewordSolver = new CodewordSolver();
-    final List<CodeWord> solvedPuzzles = codewordSolver.solve(puzzle);
-    System.out.println(solvedPuzzles.get(0).getKey());
-    System.out.println(new CodeWord(puzzle.getGrid(), solvedPuzzles.get(0).getKey()));
+    final CodeWord solved = puzzle.solve();
+    System.out.println(solved.toString());
   }
 
 }
