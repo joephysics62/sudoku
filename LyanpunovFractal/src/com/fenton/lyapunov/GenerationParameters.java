97c0806e599b894330ba208ae08c0f21f73f4e2b
joephyics62 <fentonjoe@hotmail.com>
2016-08-07 20:41:29 +0100

    Separated generation code from UI better

---
+++ LyanpunovFractal/src/com/fenton/lyapunov/GenerationParameters.java
@@ -0,0 +1,107 @@
+package com.fenton.lyapunov;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class GenerationParameters implements Serializable {
+  private static final long serialVersionUID = 5671858909961417148L;
+
+  private String lyapunovString = "AB";
+  private int maxIterations = 50;
+  private int frequency = 100;
+  private double xmin = 2;
+  private double xmax = 4;
+  private double ymin = 2;
+  private double ymax = 4;
+  private double threshold = 2.5;
+
+  private int redPhase = 20;
+  private int greenPhase = 0;
+  private int bluePhase = 80;
+
+  public byte[] getImageData() {
+    final Plot plot = new Plot(500, 500, xmin, xmax, ymin, ymax);
+    final LyapunovGenerator lyapunovGenerator = new LyapunovGenerator(lyapunovString, maxIterations);
+    plot.generateData(lyapunovGenerator);
+    final LyapunovRenderer renderer = new LyapunovRenderer(
+        frequency,
+        threshold,
+        redPhase,
+        greenPhase,
+        bluePhase);
+    try {
+      return plot.writeToBytes(renderer);
+    }
+    catch (final IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public String getLyapunovString() {
+    return lyapunovString;
+  }
+  public void setLyapunovString(final String lyapunovString) {
+    this.lyapunovString = lyapunovString;
+  }
+  public int getMaxIterations() {
+    return maxIterations;
+  }
+  public void setMaxIterations(final int maxIterations) {
+    this.maxIterations = maxIterations;
+  }
+  public int getFrequency() {
+    return frequency;
+  }
+  public void setFrequency(final int frequency) {
+    this.frequency = frequency;
+  }
+  public double getXmin() {
+    return xmin;
+  }
+  public void setXmin(final double xmin) {
+    this.xmin = xmin;
+  }
+  public double getXmax() {
+    return xmax;
+  }
+  public void setXmax(final double xmax) {
+    this.xmax = xmax;
+  }
+  public double getYmin() {
+    return ymin;
+  }
+  public void setYmin(final double ymin) {
+    this.ymin = ymin;
+  }
+  public double getYmax() {
+    return ymax;
+  }
+  public void setYmax(final double ymax) {
+    this.ymax = ymax;
+  }
+  public double getThreshold() {
+    return threshold;
+  }
+  public void setThreshold(final double threshold) {
+    this.threshold = threshold;
+  }
+  public int getRedPhase() {
+    return redPhase;
+  }
+  public void setRedPhase(final int redPhase) {
+    this.redPhase = redPhase;
+  }
+  public int getGreenPhase() {
+    return greenPhase;
+  }
+  public void setGreenPhase(final int greenPhase) {
+    this.greenPhase = greenPhase;
+  }
+  public int getBluePhase() {
+    return bluePhase;
+  }
+  public void setBluePhase(final int bluePhase) {
+    this.bluePhase = bluePhase;
+  }
+
+}
--- LyanpunovFractal/src/com/fenton/wicket/LyapunovHome.java
+++ LyanpunovFractal/src/com/fenton/wicket/LyapunovHome.java
@@ -1,7 +1,5 @@
 package com.fenton.wicket;
 
-import java.io.IOException;
-
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
@@ -9,65 +7,34 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.image.NonCachingImage;
 import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.DynamicImageResource;
 
-import com.fenton.lyapunov.LyapunovGenerator;
-import com.fenton.lyapunov.LyapunovRenderer;
-import com.fenton.lyapunov.Plot;
+import com.fenton.lyapunov.GenerationParameters;
 
 public class LyapunovHome extends WebPage {
   private static final long serialVersionUID = -2160699419531791694L;
 
-  protected String lyapunovString = "AB";
-  protected int maxIterations = 50;
-  protected int frequency = 100;
-  protected double xmin = 2;
-  protected double xmax = 4;
-  protected double ymin = 2;
-  protected double ymax = 4;
-  protected double threshold = 2.5;
-
-  protected int redPhase = 20;
-  protected int greenPhase = 0;
-  protected int bluePhase = 80;
-
-
+  protected IModel<GenerationParameters> params = Model.of(new GenerationParameters());
 
   public LyapunovHome(final PageParameters parameters) {
     add(new Label("title", "Lyapunov Generator"));
 
     final Form<?> form = new Form<Void>("form");
 
-    final IModel<String> lyapunovStringModel = new PropertyModel<>(this, "lyapunovString");
-    final IModel<Integer> maxIterationsModel = new PropertyModel<>(this, "maxIterations");
-    final IModel<Integer> frequencyModel = new PropertyModel<>(this, "frequency");
-
-    final IModel<Double> xminModel = new PropertyModel<>(this, "xmin");
-    final IModel<Double> xmaxModel = new PropertyModel<>(this, "xmax");
-    final IModel<Double> yminModel = new PropertyModel<>(this, "ymin");
-    final IModel<Double> ymaxModel = new PropertyModel<>(this, "ymax");
-
-    final IModel<Double> thresholdModel = new PropertyModel<>(this, "threshold");
-
-    final IModel<Integer> redPhaseModel = new PropertyModel<>(this, "redPhase");
-    final IModel<Integer> greenPhaseModel = new PropertyModel<>(this, "greenPhase");
-    final IModel<Integer> bluePhaseModel = new PropertyModel<>(this, "bluePhase");
-
-
-
-    form.add(new TextField<>("lyapunovString", lyapunovStringModel));
-    form.add(new TextField<>("maxIterations", maxIterationsModel));
-    form.add(new TextField<>("frequency", frequencyModel));
-    form.add(new TextField<>("xmin", xminModel));
-    form.add(new TextField<>("xmax", xmaxModel));
-    form.add(new TextField<>("ymin", yminModel));
-    form.add(new TextField<>("ymax", ymaxModel));
-    form.add(new TextField<>("threshold", thresholdModel));
-    form.add(new TextField<>("redPhase", redPhaseModel));
-    form.add(new TextField<>("greenPhase", greenPhaseModel));
-    form.add(new TextField<>("bluePhase", bluePhaseModel));
+    form.add(new TextField<>("lyapunovString", new PropertyModel<>(params, "lyapunovString")));
+    form.add(new TextField<>("maxIterations", new PropertyModel<>(params, "maxIterations")));
+    form.add(new TextField<>("frequency", new PropertyModel<>(params, "frequency")));
+    form.add(new TextField<>("xmin", new PropertyModel<>(params, "xmin")));
+    form.add(new TextField<>("xmax", new PropertyModel<>(params, "xmax")));
+    form.add(new TextField<>("ymin", new PropertyModel<>(params, "ymin")));
+    form.add(new TextField<>("ymax", new PropertyModel<>(params, "ymax")));
+    form.add(new TextField<>("threshold", new PropertyModel<>(params, "threshold")));
+    form.add(new TextField<>("redPhase", new PropertyModel<>(params, "redPhase")));
+    form.add(new TextField<>("greenPhase", new PropertyModel<>(params, "greenPhase")));
+    form.add(new TextField<>("bluePhase", new PropertyModel<>(params, "bluePhase")));
 
 
     add(form);
@@ -80,23 +47,7 @@ public class LyapunovHome extends WebPage {
           private static final long serialVersionUID = 8045959870591042005L;
 
           @Override protected byte[] getImageData(final Attributes attributes) {
-            final Plot plot = new Plot(500, 500,
-                                       xminModel.getObject(), xmaxModel.getObject(),
-                                       yminModel.getObject(), ymaxModel.getObject());
-            final LyapunovGenerator lyapunovGenerator = new LyapunovGenerator(lyapunovStringModel.getObject(), maxIterationsModel.getObject());
-            plot.generateData(lyapunovGenerator);
-            final LyapunovRenderer renderer = new LyapunovRenderer(
-                frequencyModel.getObject(),
-                thresholdModel.getObject(),
-                redPhaseModel.getObject(),
-                greenPhaseModel.getObject(),
-                bluePhaseModel.getObject());
-            try {
-              return plot.writeToBytes(renderer);
-            }
-            catch (final IOException e) {
-              throw new RuntimeException(e);
-            }
+            return params.getObject().getImageData();
           }
         };
         dir.setFormat("image/png");
